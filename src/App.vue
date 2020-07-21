fa51f71f3e842c3d4b113b5991cb4224ef84fbf8
Ivan Dezisque <dzsq1337@yandex.ru>
2020-07-21 14:50:14 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <svg>
+      <circle :cx="x1" :cy="y1" r="8" fill="green" ref="circle1" @mousedown="changePointCoords"></circle>
+      <line :x1="x1" :y1="y1" :x2="x2" :y2="y2" stroke="green" stroke-width="2"></line>
+      <circle :cx="x2" :cy="y2" r="8" fill="green" ref="circle2" @mousedown="changePointCoords"></circle>
+    </svg>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      x1: 5,
+      x2: 55,
+      y1: 5,
+      y2: 55,
+    }
+  },
+  methods: {
+    changePointCoords(){
+      window.addEventListener("mousemove", this.mousemoveHandler);
+      window.addEventListener("mouseup", this.mouseupHandler);
+    },
+    mousemoveHandler(e){
+      switch (e.srcElement) {
+        case (this.$refs.circle1):
+          this.x1 = e.offsetX;
+          this.y1 = e.offsetY;
+          break;
+        case (this.$refs.circle2):
+          this.x2 = e.offsetX;
+          this.y2 = e.offsetY;
+          break;
+      }
+    },
+    mouseupHandler(){
+      window.removeEventListener("mousemove", this.mousemoveHandler);
+      window.removeEventListener("mouseup", this.mouseupHandler);
+    }
   }
 }
 </script>
@@ -24,5 +53,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+svg{
+  border: 2px solid black;
 }
 </style>
